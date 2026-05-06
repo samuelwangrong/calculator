4e732f82c784e4990981de1730890683c42a6d8b
samuelwangrong <111710253+samuelwangrong@users.noreply.github.com>
2026-05-06 12:05:22 -0400

    finished calculator app

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,278 @@
+import { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+export default function RootLayout() {
+  const [firstNum, setFirstNum] = useState(0);
+  const [secondNum, setSecondNum] = useState(0);
+  const [currentOperator, setCurrentOperator] = useState("");
+  const [activeOperator, setActiveOperator] = useState(false);
+  const [screenVal, setScreenVal] = useState("");
+
+  const handleOperator = (operator: string) => {
+    if (operator !== currentOperator && activeOperator == false) {
+      setCurrentOperator(operator);
+      setScreenVal(operator);
+      setActiveOperator(true);
+    }
+  };
+
+  const handleNum = (num: number) => {
+    if (activeOperator == false) {
+      let temp: number = Number(String(firstNum).concat(String(num)));
+      setFirstNum(temp);
+      setScreenVal(String(temp));
+    } else {
+      let temp: number = Number(String(secondNum).concat(String(num)));
+      setSecondNum(temp);
+      setScreenVal(String(temp));
+    }
+  };
+
+  const handleEqual = () => {
+    let temp: number;
+    if (currentOperator == "+") {
+      temp = firstNum + secondNum;
+      setScreenVal(String(temp));
+    } else if (currentOperator == "-") {
+      temp = firstNum - secondNum;
+      setScreenVal(String(temp));
+    } else if (currentOperator == "/") {
+      temp = firstNum / secondNum;
+      setScreenVal(String(temp));
+    } else {
+      temp = firstNum * secondNum;
+      setScreenVal(String(temp));
+    }
+
+    setCurrentOperator("");
+    setFirstNum(0);
+    setSecondNum(0);
+    setActiveOperator(false);
+  };
+  return (
+    <SafeAreaProvider style={styles.container}>
+      <View>
+        <Text style={styles.label}>{screenVal}</Text>
+      </View>
+
+      <View style={styles.row}>
+        <TouchableOpacity
+          style={styles.oppButton}
+          onPress={() => {
+            handleNum(0);
+          }}
+        >
+          <Text style={styles.oppText}>0</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.numCon}>
+        <View style={styles.row}>
+          <TouchableOpacity
+            style={styles.numButton}
+            onPress={() => {
+              handleNum(1);
+            }}
+          >
+            <Text>1</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+          <TouchableOpacity
+            style={styles.numButton}
+            onPress={() => {
+              handleNum(2);
+            }}
+          >
+            <Text>2</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+          <TouchableOpacity
+            style={styles.numButton}
+            onPress={() => {
+              handleNum(3);
+            }}
+          >
+            <Text>3</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <View style={styles.numCon}>
+        <View style={styles.row}>
+          <TouchableOpacity
+            style={styles.numButton}
+            onPress={() => {
+              handleNum(4);
+            }}
+          >
+            <Text>4</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+          <TouchableOpacity
+            style={styles.numButton}
+            onPress={() => {
+              handleNum(5);
+            }}
+          >
+            <Text>5</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+          <TouchableOpacity
+            style={styles.numButton}
+            onPress={() => {
+              handleNum(6);
+            }}
+          >
+            <Text>6</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <View style={styles.numCon}>
+        <View style={styles.row}>
+          <TouchableOpacity
+            style={styles.numButton}
+            onPress={() => {
+              handleNum(7);
+            }}
+          >
+            <Text>7</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+          <TouchableOpacity
+            style={styles.numButton}
+            onPress={() => {
+              handleNum(8);
+            }}
+          >
+            <Text>8</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+          <TouchableOpacity
+            style={styles.numButton}
+            onPress={() => {
+              handleNum(9);
+            }}
+          >
+            <Text>9</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <View style={styles.numCon}>
+        <View style={styles.row}>
+          <TouchableOpacity
+            style={styles.oppButton}
+            onPress={() => {
+              handleOperator("+");
+            }}
+          >
+            <Text style={styles.oppText}>+</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+          <TouchableOpacity
+            style={styles.oppButton}
+            onPress={() => {
+              handleOperator("-");
+            }}
+          >
+            <Text style={styles.oppText}>-</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+          <TouchableOpacity
+            style={styles.oppButton}
+            onPress={() => {
+              handleOperator("*");
+            }}
+          >
+            <Text style={styles.oppText}>x</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+          <TouchableOpacity
+            style={styles.oppButton}
+            onPress={() => {
+              handleOperator("/");
+            }}
+          >
+            <Text style={styles.oppText}>÷</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={styles.row}>
+        <TouchableOpacity
+          style={styles.oppButton}
+          onPress={() => {
+            handleEqual();
+          }}
+        >
+          <Text style={styles.oppText}>=</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaProvider>
+  );
+}
+{
+}
+
+const styles = StyleSheet.create({
+  label: {
+    fontSize: 30,
+  },
+  container: {
+    padding: 20,
+    flexDirection: "column",
+  },
+  row: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  numButton: {
+    width: 100,
+    height: 50,
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+    borderRadius: 4,
+    backgroundColor: "oldlace",
+    marginHorizontal: "3%",
+    marginBottom: 6,
+    textAlign: "center",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  numCon: {
+    flexDirection: "row",
+  },
+  oppButton: {
+    width: 70,
+    height: 50,
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+    borderRadius: 4,
+    backgroundColor: "oldlace",
+    marginHorizontal: "3%",
+    marginBottom: 6,
+    textAlign: "center",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  oppText: {
+    fontSize: 20,
+  },
+});
+
+/**
+ * CALCULATOR FUNCTIONALITY
+ *if previous number doesn't exist, operator key should ignore it,
+ *
+ * after equal how handle the logic?
+ *    if after equal, store the previous and add to a second operator is fine
+ */
---
+++ app/_layout.tsx
@@ -0,0 +1,3 @@
+export const unstable_settings = {
+  anchor: "(tabs)",
+};
